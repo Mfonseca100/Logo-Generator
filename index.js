d2776893af1a0ee078f72d8f23ddabc00af6dac7
Edith <rissafonseca@yahoo.com>
2023-05-01 17:35:11 -0500

    added init function

--- index.js
+++ index.js
@@ -1,64 +1,102 @@
-const inquirer = require('inquirer')
-const {Circle, Square, Triangle} = require("./shapes");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Circle, Square, Triangle } = require("./lib/shapes");
 
-class SVG{
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
+class SVG {
+    constructor() {
+        this.textElement = '';
+        this.shapeElement = '';
     }
-    render(){
+    render() {
         return `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}
-        </svg>`
+          </svg>`;
     }
-    setTextElement(text, color){
-        this.textElement = `<text x= "150" y="125" font-size= "60" text-anchor="middle">`
+    setTextElement(text, color = 'black') {
+        this.textElement = `<text x= "150" y="125" font-size= "60" text-anchor="middle" fill="${color}">${text}</text>`;
     }
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
-    
+    setShapeElement(shape, color = 'black') {
+        shape.setColor(color);
+        this.shapeElement = shape.render();
     }
 }
 
-
-//an array of questions for user input 
+//an array of questions for user input
 const questions = [
-{
-    type: "input",
-    message: "text",
-    name: "Enter up to (3) Characters:", 
-},
-{
-    type: "input", 
-    message: "text-color",
-    name: "TEXT COLOR: Enter a color keyword (OR hexadecimal number):", 
-}, 
-{
-    type: "input", 
-    message: "shape", 
-    name: "SHAPE COLOR: Enter a color keyword (OR hexadecimal number):",
-},
-{
-    type: "list",
-    message: "pixel image",
-    name: "Choose what pixel image you would like?",
-    choices: [
-        "Circle", 
-        "Square", 
-        "Triangle"
-    ]
-}
+    {
+        type: "input",
+        name: "text",
+        message: "Enter no more than 3 Characters:",
+        validation: (value) => {
+            if (value.length <= 3) {
+                return true;
+            } else {
+                return "Characters must be no more than 3 characters";
+            }
+        },
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "TEXT COLOR: Enter a color keyword (OR hexadecimal number):",
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "SHAPE COLOR: Enter a color keyword (OR hexadecimal number):",
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "What is your image of choice?",
+        choices: ["Circle", "Square", "Triangle"],
+    },
 ];
 
-function init() {
-    inquirer
-        .prompt(questions)
-        .then(data => {
-            const SVG = new SVG();
-            SVG.setTextElement(data.text, data.color.toLowerCase());
-            SVG.setShapeElement(data.image);
-            const writeSVG = `
-            ${SVG.render()}
-            `
-            writeToFile(writeSVG, data.text);
-        })
+// Function to write data to file
+function writeToFile(fileName, data) {
+    console.log(`Writing [${data}] to file [${fileName}]`);
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Congratulations, you have Generated a logo.svg!");
+    });
 }
+
+async function init() {
+    console.log("Starting init");
+
+    const svg = new SVG();
+
+    const answers = await inquirer.prompt(questions);
+    console.log("User's answers:", answers);
+
+    const { text, textColor, shapeColor, shape } = answers;
+
+    svg.setTextElement(text, textColor);
+
+    let shapeObj;
+
+    switch (shape) {
+        case "Circle":
+            shapeObj = new Circle(shapeColor);
+            break;
+        case "Square":
+            shapeObj = new Square(shapeColor);
+            break;
+        case "Triangle":
+            shapeObj = new Triangle(shapeColor);
+            break;
+        default:
+            shapeObj = new Circle(shapeColor);
+            break;
+    }
+
+    svg.setShapeElement(shapeObj);
+
+    const svgString = svg.render();
+
+    writeToFile("logo.svg", svgString);
+}
+
+init();
